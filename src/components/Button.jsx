ff92564116cc756f460837cecae65b108127c7a9
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-05-19 18:18:10 +0530

    final commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-
-  const spanClasses = `relative z-10`;
+  const spanClasses = "relative z-10";
 
   const renderButton = () => (
     <button className={classes} onClick={onClick}>
